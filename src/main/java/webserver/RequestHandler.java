9ec5674feb3d909a6e424c9b7aa8de8a6770f2dc
krSeonghyeon <ya41921174@gmail.com>
2025-12-30 11:15:42 +0900

    feat: 기능요구사항 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,8 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,10 +20,29 @@ public class RequestHandler implements Runnable {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "<h1>Hello World</h1>".getBytes();
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream();
+             BufferedReader br = new BufferedReader(new InputStreamReader(in));
+             DataOutputStream dos = new DataOutputStream(out);
+        ) {
+            String requestLine = br.readLine();
+            if (requestLine == null || requestLine.isEmpty()) {
+                return;
+            }
+
+            String[] tokens = requestLine.split(" ");
+            String url = tokens[1];
+
+            StringBuilder requestLog = new StringBuilder();
+            requestLog.append(requestLine).append("\n");
+            while ((requestLine = br.readLine()) != null && !requestLine.isEmpty()) {
+                requestLog.append(requestLine).append("\n");
+            }
+            logger.debug(requestLog.toString());
+
+            File file = new File("src/main/resources/static" + url);
+            byte[] body = Files.readAllBytes(file.toPath());
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
