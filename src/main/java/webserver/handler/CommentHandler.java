4c4ec5a999b29897779ad8f3daaebeeb29f75822
krSeonghyeon <ya41921174@gmail.com>
2026-01-14 18:26:39 +0900

    fix: 헤더 요구사항 반영

--- src/main/java/webserver/handler/CommentHandler.java
+++ src/main/java/webserver/handler/CommentHandler.java
@@ -1,10 +1,17 @@
 package webserver.handler;
 
+import model.user.User;
+import webserver.argument.annotation.CurrentUser;
 import webserver.view.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CommentHandler implements Handler {
 
-    public ModelAndView handle() {
-        return new ModelAndView("comment", null);
+    public ModelAndView handle(@CurrentUser User user) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("username", user.getName());
+        return new ModelAndView("comment", model);
     }
 }
--- src/main/java/webserver/handler/MyPageHandler.java
+++ src/main/java/webserver/handler/MyPageHandler.java
@@ -1,10 +1,17 @@
 package webserver.handler;
 
+import model.user.User;
+import webserver.argument.annotation.CurrentUser;
 import webserver.view.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyPageHandler implements Handler {
 
-    public ModelAndView handle() {
-        return new ModelAndView("mypage", null);
+    public ModelAndView handle(@CurrentUser User user) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("username", user.getName());
+        return new ModelAndView("mypage", model);
     }
 }
