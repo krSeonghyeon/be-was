5414744f629bb1ef83c94d996d56f93906187614
krSeonghyeon <ya41921174@gmail.com>
2026-01-14 17:18:04 +0900

    feat: sql 초기화 및 DB 유지 추가

--- src/main/java/db/ConnectionManager.java
+++ src/main/java/db/ConnectionManager.java
@@ -3,13 +3,36 @@ package db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConnectionManager {
+
+    static {
+        try (Connection conn = ConnectionManager.getConnection();
+             Statement stmt = conn.createStatement()) {
+
+            stmt.execute("""
+                    CREATE TABLE USERS (
+                        user_id VARCHAR(50) PRIMARY KEY,
+                        password VARCHAR(100),
+                        name VARCHAR(100),
+                        email VARCHAR(100))
+                    """);
+            stmt.execute("""
+                    CREATE TABLE ARTICLES (
+                        article_id VARCHAR(50) PRIMARY KEY,
+                        content CLOB)
+                    """);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(
-                    "jdbc:h2:mem:testdb",
+                    "jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
                     "sa",
                     ""
             );
